3f0422bd3666b96e8e639d2cfa2e926f00ad4707
Arracias <altrishka@gmail.com>
2018-12-01 11:51:08 +0300

    Basic defender and attacker + UI

---
+++ Garden Mayhem/Assets/Scripts/Defender.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defender : MonoBehaviour {
+
+    public void AddStarsEvent(int amount)
+    {
+        FindObjectOfType<StarDisplay>().AddStars(amount);
+    }
+
+}
--- Garden Mayhem/Assets/Scripts/LevelLoad.cs
+++ Garden Mayhem/Assets/Scripts/LevelLoad.cs
@@ -1,31 +1,35 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelLoader : MonoBehaviour
-{
-    int currentSceneIndex;
-    [SerializeField] int timeToWait = 4;
+public class LevelLoad : MonoBehaviour {
 
-    void Start()
-    {
+    [SerializeField] int timeToWait = 4;
+    int currentSceneIndex;
+
+	// Use this for initialization
+	void Start () {
         currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         if (currentSceneIndex == 0)
         {
             StartCoroutine(WaitForTime());
         }
-    }
+	}
 
-    private IEnumerator WaitForTime()
+    IEnumerator WaitForTime()
     {
         yield return new WaitForSeconds(timeToWait);
         LoadNextScene();
     }
-
+	
     public void LoadNextScene()
     {
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
